58a2656f054b0d99c40b131346a7e569f7692d5a
yuval-ro <yuvalrot@m.telhai.ac.il>
2023-01-21 18:03:29 +0200

    asdf

--- src/components/MessageForm/MessageForm.js
+++ src/components/MessageForm/MessageForm.js
@@ -2,64 +2,40 @@ import React, { useState } from 'react';
 import { sendMessage } from '../../Api';
 
 import './MessageForm.css';
-import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 
 
 
-const MessageForm = () => {
-	const [message, setMessage] = useState('');
-	const [response, setResponse] = useState('');
+const MessageForm = (props) => {
 
-	const handleSubmit = e => {
+	const handleSubmit = (e) => {
 		e.preventDefault();
-		sendMessage(message)
-			.then((response) => {
-				setResponse(response);
+		sendMessage(props.message)
+			.then((res) => {
+				props.setResponse(res);
 			})
 			.catch((err) => {
 				console.error(err);
 			});
 	};
 
-	const onMessageFormChange = e => {
-		setMessage(e?.target?.value);
+	const onMessageFormChange = (e) => {
+		e.preventDefault();
+		props.setMessage(e?.target?.value);
 	}
 
 	return (
-		<React.Fragment>
-			<Row className='mt-5 mb-5 border border-1'>
-				<Col>
-					<Row className='justify-content-start'>
-						<Col className='col-5 border border-primary rounded-1'>
-							What is your question?
-						</Col>
-					</Row>
-					<Row className='justify-content-end'>
-						<Col className='col-5 border border rounded-2'>
-							My Question is... ?
-						</Col>
-					</Row>
-					<Row className='justify-content-start'>
-						<Col className='col-5 border border-primary rounded-1'>
-							Okay, this is my answer...
-						</Col>
-					</Row>
-				</Col>
-			</Row>
-
-			<Row className='mt-5 mb-5 border border-1'>
-				<Col>
-					<Form onSubmit={handleSubmit}>
-						<Form.Group className='' controlId=''>
-							<Form.Control type='text' placeholder='Enter your question here'
-								value={message}
-								onChange={e => onMessageFormChange(e)}></Form.Control>
-						</Form.Group>
-						<Button variant='primary' type='submit'>Submit</Button>
-					</Form>
-				</Col>
-			</Row>
-		</React.Fragment>
+		<Row className='mt-5 mb-5 border border-1'>
+			<Col>
+				<Form onSubmit={handleSubmit}>
+					<Form.Group className='' controlId=''>
+						<Form.Control type='text' placeholder='Ask me anything...'
+							onChange={e => onMessageFormChange(e)} ></Form.Control>
+					</Form.Group>
+					<Button variant='primary' type='submit'>Submit</Button>
+				</Form>
+			</Col>
+		</Row>
 	);
 }
 
--- src/containers/Home/Home.js
+++ src/containers/Home/Home.js
@@ -1,19 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Home.css';
 import { Container, Row, Col } from 'react-bootstrap';
 import MessageForm from '../../components/MessageForm/MessageForm';
+import MessageWindow from '../../components/MessageWindow/MessageWindow';
 
+const Home = () => {
 
-const Home = () => (
-	<Container className='container-fluid border border-3'>
-		<Row className='mt-5 mb-5 border border-1'>
-			<Col className='fs-1'>
-				Chat with Davinci
-			</Col>
-		</Row>
+	const [message, setMessage] = useState('');
+	const [response, setResponse] = useState('');
 
-		<MessageForm />
-	</Container>
-);
+	return (
+		<Container className='container-fluid border border-3'>
+			<Row className='mt-5 mb-5 border border-1'>
+				<Col className='fs-1'>
+					Chat with Davinci
+				</Col>
+			</Row>
+
+			<MessageWindow />
+
+			<MessageForm
+				message={message}
+				setMessage={setMessage}
+				response={response}
+				setResponse={setResponse}
+			/>
+		</Container>
+	)
+}
 
 export default Home;
