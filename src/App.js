034ba992d4cbd869da5f56e4f3a72c63ba58b356
yuval-ro <yuvalrot@m.telhai.ac.il>
2023-01-22 23:20:57 +0200

    asdf

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+import { AuthProvider, RequireAuth } from 'react-auth-kit';
+
 import './App.css';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import Home from './containers/Home/Home';
 import Admin from './containers/Admin/Admin';
 import Login from './containers/Login/Login';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { AuthProvider, RequireAuth } from 'react-auth-kit';
 import About from './containers/About/About';
 import NotFound from './containers/NotFound/NotFound';
+
 
 const App = () => {
 	const [isLoggedIn, setIsLoggedIn] = useState(false); // 'Logout' Button functionality
@@ -42,8 +44,12 @@ const App = () => {
 								<Admin />
 							</RequireAuth>} />
 					<Route
-						path='/*'
+						path='/404'
 						element={<NotFound />}
+					/>
+					<Route
+						path='/*'
+						element={<Navigate to='/404' />}
 					/>
 				</Routes>
 				<Footer />
