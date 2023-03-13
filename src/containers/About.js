f0f0e769b6237f3152bd58a315bb58b0e563317a
yuval-ro <yuvalro@protonmail.com>
2023-03-13 12:10:56 +0200

    Changed About page

--- src/containers/About.js
+++ src/containers/About.js
@@ -13,24 +13,7 @@ const About = () => (
     </Row>
     <Row>
       <Col>
-        <p>
-          This is our project for the course;<br />
-          The entire source code is original as well as the design!
-        </p>
-        <p>
-          This project consists of a React app as the front-end,
-          and an Express server as the back-end.<br />
-          Authorized users can login as administrators,
-          and observe the logs of previous correspondance made.
-        </p>
-        <p>
-          The main event is chatting with the Davinci model.<br />
-          Each prompt will send a request to OpenAI's API endpoint;<br />
-          Afterwards, the reponse will be recorded as a log in the Mongo database, then displayed in the chat window.<br />
-        </p>
-        <p>
-          Hope you enjoy your stay, feel free to wander around the site!
-        </p>
+        About this app...
       </Col>
     </Row>
   </Container>
