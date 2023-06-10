e24080682a0b374b207d5309897fbacabf251921
yuval-ro <yuvalro@protonmail.com>
2023-06-10 17:48:49 +0300

    Updated entire repo.

---
+++ server/secret.ts
@@ -0,0 +1,13 @@
+const user: string = 'PLACEHOLDER';
+const pass: string = 'PLACEHOLDER';
+const secret: string = 'PLACEHOLDER';
+const org: string = 'PLACEHOLDER';
+const key: string = 'PLACEHOLDER';
+
+module.exports = {
+  user,
+  pass,
+  secret,
+  org,
+  key
+}
